fcf175bf9493f22424a2912b03a37db983d85a4c
HiagoGGA <hiagogabriel1132@gmail.com>
2024-05-28 22:03:09 -0400

    oi

---
+++ JavaScript/AtividadeCursoEmVideo/aula11/ex008.js
@@ -0,0 +1,9 @@
+var velocidade = 120.5
+
+console.log(`A velociade do seu carro é ${velocidade}`)
+
+if (velocidade > 60) {
+    console.log(`Você ultrapassou o limite de velocidade.`)
+}
+
+console.log("Dirija sempre com sinto de segurança!")
---
+++ JavaScript/AtividadeCursoEmVideo/aula12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 15
+if (idade < 16) {
+    console.log("Não vota")
+} else {
+    if (idade >= 16 && idade <= 17 || idade > 64) {
+        console.log("Voto opcional")
+    } else console.log("voto obrigatório")
+} 
+
