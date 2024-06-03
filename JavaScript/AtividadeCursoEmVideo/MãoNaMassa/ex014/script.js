3383e0bb1170f9369833e326d0253613da3f2f88
HiagoGGA <hiagogabriel1132@gmail.com>
2024-06-03 16:30:38 -0400

    a

---
+++ JavaScript/AtividadeCursoEmVideo/MãoNaMassa/ex014/script.js
@@ -0,0 +1,21 @@
+function carregar() {
+    var msg = window.document.getElementById("msg")
+    var img = window.document.getElementById("imagem")
+    var data = new Date()
+    var hora = 19
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        img.src = "img/manha.jpg"
+        msg.innerHTML += `, Bom Dia`
+        document.body.style.background = "rgb(224, 189, 71)"
+
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = "img/tarde.jpg"
+        msg.innerHTML += `, Boa Tarde`
+        document.body.style.background = "rgb(255, 137, 26)"
+    } else {
+        img.src = "img/noite.jpg"
+        msg.innerHTML += `, Boa Noite`
+        document.body.style.background = "rgb(34, 73, 131)"
+    }
+}
